c765370c9231f94c07682a39d91e56def32f6a12
PeterHedleyJHA <phedley@jha.com>
2019-04-11 10:49:42 +0100

    added yml file

--- tests/test_christmas.py
+++ tests/test_christmas.py
@@ -1,4 +1,5 @@
 import unittest
+
 from no_days_christmas.attempt import get_no_presents_from_true_love
 
 class ChristmasTest(unittest.TestCase):
@@ -8,3 +9,8 @@ class ChristmasTest(unittest.TestCase):
                364, 455, 560, 680, 816, 969, 1140, 1330, 1540]
         for no_christmas_days,answer in enumerate(ans):
             self.assertEqual(get_no_presents_from_true_love(no_christmas_days+1),answer)
+
+
+if __name__=="__main__":
+    ct = ChristmasTest()
+    ct.test_true_love_presents()
