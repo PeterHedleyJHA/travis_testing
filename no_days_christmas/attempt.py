111016dd550a01230ff9d2a3740c236a1102f337
PeterHedleyJHA <phedley@jha.com>
2019-04-11 12:24:08 +0100

    removed linting

--- no_days_christmas/attempt.py
+++ no_days_christmas/attempt.py
@@ -1,16 +1,16 @@
-def get_no_presents_from_true_love(target_n,n=1,pt1=0,pt2=0):
+def get_no_presents_from_true_love(target_n, n=1, pt1=0, pt2=0):
     if target_n==n:
         return n+2*pt1+pt2
-    return get_no_presents_from_true_love(target_n,n+1,pt1+n,pt1+pt2)
+    return get_no_presents_from_true_love(target_n, n+1, pt1+n, pt1+pt2)
 
 
 def bubble_sort(array):
     start = 0
     finished=False
-    while(finished==False):
+    while not finished:
         finished = True
         for index in range(len(array)-1):
-            if(array[index] < array[index+1]):
+            if array[index] < array[index+1]:
                 temp = array[index]
                 array[index] = array[index+1]
                 array[index+1] = temp
@@ -23,5 +23,5 @@ if __name__ == "__main__":
         ans.append(get_no_presents_from_true_love(i+1))
     print(ans)
 
-    print(bubble_sort([3,4,5]))
-    print(bubble_sort([3,4,5,2,3,3,4,4,3,35,3,4,9,10,11,2,3,2,43]))
+    print(bubble_sort([3, 4, 5]))
+    print(bubble_sort([3, 4, 5, 2, 3, 3, 4, 4, 3, 35, 3, 4, 9, 10, 11, 2, 3, 2, 43]))
